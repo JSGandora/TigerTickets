b8b35a47fe896582be7aa9aa70dba00eb4ba019f
Henry Birge-Lee <henrybirgelee@gmail.com>
2017-04-03 22:31:13 -0400

    Added logout.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
+  # Disables a poerful security feature but is needed for debugging. With this line, this controller is voulnerable to XSRF attcks.
+  skip_before_action :verify_authenticity_token
   before_filter CASClient::Frameworks::Rails::Filter
   def index
+  end
+  def logout
+    CASClient::Frameworks::Rails::Filter.logout(self)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'buy', to: 'buy#buyrequest'
   post 'cancel-buy', to: 'buy#cancelbuy'
   get 'shows', to: 'shows_view#getshows'
+  post 'logout', to: 'welcome#logout'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
