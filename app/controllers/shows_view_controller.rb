fb6fd44e5a4503324bf6ec456626eb6c377582ff
Henry Birge-Lee <henrybirgelee@gmail.com>
2017-04-06 19:39:44 -0400

    Changed shows to epoch time.

--- app/controllers/shows_view_controller.rb
+++ app/controllers/shows_view_controller.rb
@@ -1,7 +1,11 @@
 class ShowsViewController < ApplicationController
   def getshows
-  	@shows = Show.all
-    response = { :status => "ok", :shows => @shows}
+  	shows = Show.all
+    showsResponse = []
+    for show in shows
+      showsResponse << {:id => show['id'], :title => show['title'], :time => show['time'].to_i, :location => show['location'], :group => show['group']}
+    end
+    response = { :status => "ok", :shows => showsResponse}
     render json: response
   end
 end
