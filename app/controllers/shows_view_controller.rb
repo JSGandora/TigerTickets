cc69a7d5f3830253bae371eadd79f0a048df13d1
Richard Du <rdu@princeton.edu>
2017-04-12 02:55:29 +0000

    Fix show data

--- app/controllers/shows_view_controller.rb
+++ app/controllers/shows_view_controller.rb
@@ -5,7 +5,7 @@ class ShowsViewController < ApplicationController
     for show in shows
       showsResponse << {:id => show['id'], :name => show['title'], :time => show['time'].to_i, 
         :location => show['location'], :group => show['group'], 
-        :image => 'https://tickets.princeton.edu/ArticleMedia/Images/Muslim%20Alumni%20Event.jpg',
+        :image => show['img'],
         :buyreq => 0,
         :sellreq => 1,
         :price => 0}
