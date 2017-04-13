8541ffea5716494f29c455e434c73b165a7ccb6c
Henry Birge-Lee <henrybirgelee@gmail.com>
2017-04-12 23:53:05 -0400

    Changed my-tix-data api.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,13 +2,46 @@ class AccountController < ApplicationController
   before_action CASClient::Frameworks::Rails::Filter
   def mytix
     netid = session[:cas_user]
-    buyRequests = BuyRequest.where(netid: netid).where(:status => ["waiting-for-match", "pending", "completed"]).joins("INNER JOIN shows ON shows.id = buy_requests.show_id")
-    sellRequests = SellRequest.where(netid: netid).where(:status => ["waiting-for-match", "pending", "completed"]).joins("INNER JOIN shows ON shows.id = sell_requests.show_id")
+    buyRequests = BuyRequest.select("show_id, title, shows.time AS time, location, `group`, img, buy_requests.id AS buy_request_id, status").where(netid: netid).where(:status => ["waiting-for-match", "pending", "completed"]).joins("INNER JOIN shows ON shows.id = buy_requests.show_id")
+    sellRequests = SellRequest.select("show_id, title, shows.time AS time, location, `group`, img, sell_requests.id AS sell_request_id, status").where(netid: netid).where(:status => ["waiting-for-match", "pending", "completed"]).joins("INNER JOIN shows ON shows.id = sell_requests.show_id")
 
-    # This should not cause duplicate show IDs because you cannot have a buy and a sell request for the same show.
-    showIDs = buyRequests.distinct.pluck(:show_id) + sellRequests.distinct.pluck(:show_id)
-    shows = Show.where(:id => showIDs)
-    response = { :status => "ok", :netid => netid, :buyrequests => buyRequests, :sellrequests => sellRequests, :shows => shows}
+
+
+
+
+
+    buyRequestResponse = []
+    for buyRequest in buyRequests
+      show = {:id => buyRequest['show_id'], :name => buyRequest['title'], :time => (DateTime.parse(buyRequest['time'])).to_i, 
+        :location => buyRequest['location'], :group => buyRequest['group'], 
+        :image => buyRequest['img'],
+        :price => 0}
+
+      buyRequestResponse << {
+        :id => buyRequest['buy_request_id'],
+        :netid => netid,
+        :status => buyRequest['status'],
+        :show => show
+      }
+    end
+
+    sellRequestResponse = []
+    for sellRequest in sellRequests
+      show = {:id => sellRequest['show_id'], :name => sellRequest['title'], :time => (DateTime.parse(sellRequest['time'])).to_i, 
+        :location => sellRequest['location'], :group => sellRequest['group'], 
+        :image => sellRequest['img'],
+        :price => 0}
+
+      sellRequestResponse << {
+        :id => sellRequest['sell_request_id'],
+        :netid => netid,
+        :status => sellRequest['status'],
+        :show => show
+      }
+    end
+
+
+    response = { :status => "ok", :netid => netid, :buyrequests => buyRequestResponse, :sellrequests => sellRequestResponse}
     render json: response
   end
 end
