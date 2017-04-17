80f3faf81a1b5df1c4b7bb588b355a2162d0fc42
Henry Birge-Lee <henrybirgelee@gmail.com>
2017-04-16 21:44:42 -0400

    Fixed show vanishing problem maybe.

--- app/controllers/shows_view_controller.rb
+++ app/controllers/shows_view_controller.rb
@@ -1,13 +1,14 @@
 class ShowsViewController < ApplicationController
   def getshows
-  	shows = Show.select('count(sell_requests.show_id) as sell_request_count, count(buy_requests.show_id) as buy_request_count, shows.*')
+  	shows = Show.select('SUM(CASE WHEN sell_requests.status = \'waiting-for-match\' THEN 1 ELSE 0 END) AS sell_request_count, SUM(CASE WHEN buy_requests.status = \'waiting-for-match\' THEN 1 ELSE 0 END) as buy_request_count, shows.*')
       .where(["time > ?", Time.current])
       .joins('LEFT JOIN sell_requests ON shows.id = sell_requests.show_id')
       .joins('LEFT JOIN buy_requests ON shows.id = buy_requests.show_id')
       .group('shows.id')
-      .where('buy_requests.status IS NULL OR buy_requests.status = ?', 'waiting-for-match')
-      .where('sell_requests.status IS NULL OR sell_requests.status = ?', 'waiting-for-match')
       .order(:time)
+      #.where('buy_requests.status IS NULL OR buy_requests.status = ?', 'waiting-for-match')
+      #.where('sell_requests.status IS NULL OR sell_requests.status = ?', 'waiting-for-match')
+      
     showsResponse = []
     for show in shows
       showsResponse << {:id => show['id'], :name => show['title'], :time => show['time'].to_i, 
