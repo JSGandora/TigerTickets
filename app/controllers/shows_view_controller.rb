b04366ecda29a8bb9885d7b3a50dd9050b5588b1
Henry Birge-Lee <henrybirgelee@gmail.com>
2017-04-20 00:47:47 -0400

    Fixed SQL.

--- app/controllers/shows_view_controller.rb
+++ app/controllers/shows_view_controller.rb
@@ -1,18 +1,25 @@
 class ShowsViewController < ApplicationController
   def getshows
-  	shows = Show.select('SUM(CASE WHEN sell_requests.status = \'waiting-for-match\' THEN 1 ELSE 0 END) AS sell_request_count, SUM(CASE WHEN buy_requests.status = \'waiting-for-match\' THEN 1 ELSE 0 END) as buy_request_count, shows.*')
+  	shows = Show.select('SUM(CASE WHEN sell_requests.status = \'waiting-for-match\' THEN 1 ELSE 0 END) AS sell_request_count, shows.*')
       .where(["time > ?", Time.current])
       .joins('LEFT JOIN sell_requests ON shows.id = sell_requests.show_id')
+      .group('shows.id')
+      .order(:time)
+
+
+      shows2 = Show.select('SUM(CASE WHEN buy_requests.status = \'waiting-for-match\' THEN 1 ELSE 0 END) AS buy_request_count')
+      .where(["time > ?", Time.current])
       .joins('LEFT JOIN buy_requests ON shows.id = buy_requests.show_id')
       .group('shows.id')
       .order(:time)
       
     showsResponse = []
-    for show in shows
+    for i in 0..(shows.length - 1)
+      show = shows[i]
       showsResponse << {:id => show['id'], :name => show['title'], :time => show['time'].to_i, 
         :location => show['location'], :group => show['group'], 
         :image => show['img'],
-        :buyreq => show['buy_request_count'],
+        :buyreq => shows2[i]['buy_request_count'],
         :sellreq => show['sell_request_count'],
         :price => 0}
     end
