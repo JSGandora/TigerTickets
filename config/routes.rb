fdb14ecf859651b6fcc5752942538c68c838a3a5
Richard Du <rdu@princeton.edu>
2017-04-23 21:44:40 +0000

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
+  # Display my tickets
   get 'my-tix-data', to: 'account#mytix'
-  get 'my-tix', to: 'welcome#mytix'
+  
+  # Sell request API
   post 'sell', to: 'sell#sellrequest'
   post 'cancel-sell', to: 'sell#cancelsell'
+  
+  # Buy request API
   post 'buy', to: 'buy#buyrequest'
   post 'cancel-buy', to: 'buy#cancelbuy'
+  
+  # Shows API
   get 'shows', to: 'shows_view#getshows'
+  
+  # Front page
   get 'calendar', to: 'welcome#calendar'
   post 'logout', to: 'welcome#logout'
   get 'login', to: 'welcome#login'
+  get 'my-tix', to: 'welcome#mytix'
+  
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
