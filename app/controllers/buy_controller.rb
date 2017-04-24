3f05e15e35da0702d1d1d0c0ec79abd99d169e0d
Richard Du <rdu@princeton.edu>
2017-04-24 22:49:46 +0000

    Fix buy controller bug

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -2,7 +2,6 @@ class BuyController < ApplicationController
   # Disables a poerful security feature but is needed for debugging. With this line, this controller is voulnerable to XSRF attcks.
   skip_before_action :verify_authenticity_token
   before_action CASClient::Frameworks::Rails::Filter, :except => :complete_buy
-  before_action CASClient::Frameworks::Rails::GatewayFilter, :only => :complete_buy
   
   def buyrequest
     # This is the action exposed with POST /buy
