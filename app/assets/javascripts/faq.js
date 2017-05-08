5944561ddaca57559a44c579b50cd00c2a292b09
Jason Shi <jason.g.shi@gmail.com>
2017-05-08 18:14:14 -0400

    Remove console.log in faq.js

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,7 +1,6 @@
 //Get the netid of the user and renders the Login/Logout butto accordingly.
 $.get("/shows", function(response){
 	netid = response["netid"];
-		console.log(netid)
 	var text = "<a id='logout' href='javascript:void(0)'> Logout ("+netid+") </a>"
 	if(netid == null){
 		text = "<a href='login'> Login </a>"
